b4e3a1a443a6229a0d1ede7faa99954eebc16b21
JeffersonTeixeira21 <87997413+JeffersonTeixeira21@users.noreply.github.com>
2022-01-19 06:47:16 -0300

    commit de estruturação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModelModule } from './model/model.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ModelModule
   ]
 })
 export class CoreModule { }
